3365d802605ef8882a2813998ff84ecd4630b862
pushkar.shankar <pushkar.shankar@cgi.com>
2019-07-22 14:10:41 +0530

    Added FindName Logic

--- src/main/java/com/stackroute/pe4/FindName.java
+++ src/main/java/com/stackroute/pe4/FindName.java
@@ -2,6 +2,13 @@ package com.stackroute.pe4;
 
 public class FindName {
     public String nameFinder(String input){
-        return null;
-    }
-}
+        String sendBack=new String();
+        if(input!=null){
+        boolean flag=input.contains("Harry");
+        sendBack="Is Harry here ? "+flag;
+
+    }else {
+          sendBack="String is Null";
+        }
+        return sendBack;
+}}
