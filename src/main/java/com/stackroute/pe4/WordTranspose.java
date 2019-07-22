552f8f9f2b4ecce3fbc5c1cc4fa343e9d2f7eb66
pushkar.shankar <pushkar.shankar@cgi.com>
2019-07-22 14:02:00 +0530

    Added WordTranspose Logic

--- src/main/java/com/stackroute/pe4/WordTranspose.java
+++ src/main/java/com/stackroute/pe4/WordTranspose.java
@@ -1,7 +1,29 @@
 package com.stackroute.pe4;
 
+import java.lang.reflect.Array;
+import java.util.Arrays;
+
 public class WordTranspose {
-    public String wordTransposer(String Input){
-        return null;
+    public String wordTransposer(String input) {
+        if (input != null) {
+            String[] getWords = input.split(" ");
+            String sendBack = new String();
+            for (int i = 0; i < getWords.length; i++) {
+                StringBuilder temporary = new StringBuilder();
+                temporary.append(getWords[i]);
+                temporary.reverse();
+                getWords[i] = temporary.toString();
+//            System.out.println(getWords[i]);
+            }
+            for (int i = 0; i < getWords.length; i++) {
+                sendBack = sendBack.concat(getWords[i] + " ");
+
+            }
+            return sendBack.trim();
+        } else {
+            return null;
+        }
+
+//        return Arrays.toString(getWords);
     }
 }
