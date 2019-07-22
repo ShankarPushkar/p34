c43dc7571855ef8d8e03f46ce727e13f52612789
pushkar.shankar <pushkar.shankar@cgi.com>
2019-07-22 14:56:44 +0530

    modified String Occurrence Logic

--- src/main/java/com/stackroute/pe4/StringOccurrence.java
+++ src/main/java/com/stackroute/pe4/StringOccurrence.java
@@ -5,15 +5,20 @@ import java.util.regex.Matcher;
 
 public class StringOccurrence {
     public String stringOccurrenceFinder(String input, String inputWord) {
+        String sendBack = "";
         if (input != null && inputWord != null) {
             int count = 0;
             Pattern pattern = Pattern.compile(inputWord);
             Matcher matcher = pattern.matcher(input);
             boolean matches = matcher.matches();
+            if (!matcher.find()) {
+                sendBack = "Found:0 time";
+            }
             while (matcher.find()) {
                 count++;
+                sendBack += "Founded at: " + matcher.start() + "-" + matcher.end() + " ";
             }
-            return "Found:" + count + " time";
+            return sendBack.trim();
         } else {
             return null;
         }
--- src/test/java/com/stackroute/pe4/StringOccurrenceTest.java
+++ src/test/java/com/stackroute/pe4/StringOccurrenceTest.java
@@ -8,41 +8,44 @@ import static org.junit.Assert.*;
 
 public class StringOccurrenceTest {
     private StringOccurrence stringOccurrence;
+
     @Before
-    public void setUp(){
-        stringOccurrence=new StringOccurrence();
+    public void setUp() {
+        stringOccurrence = new StringOccurrence();
     }
 
     @After
-    public void tearDown(){
-        stringOccurrence=null;
+    public void tearDown() {
+        stringOccurrence = null;
     }
 
     @Test
     public void givenAStringShouldReturnWordOccurrence() {
-        String input="She sells seashells by the seashore";
-        String inputWord="se";
-        String actualResult=stringOccurrence.stringOccurrenceFinder(input,inputWord);
-        String expectedResult="Found:3 time";
-        assertEquals(expectedResult,actualResult);
+        String input = "She sells seashells by the seashore";
+        String inputWord = "se";
+        String actualResult = stringOccurrence.stringOccurrenceFinder(input, inputWord);
+        String expectedResult = "Founded at: 4-6 Founded at: 10-12 Founded at: 27-29";
+        assertEquals(expectedResult, actualResult);
 
     }
+
     @Test
     public void givenANullStringShouldReturnNull() {
-        String input=null;
-        String inputWord="se";
-        String actualResult=stringOccurrence.stringOccurrenceFinder(input,inputWord);
-        String expectedResult=null;
-        assertEquals(expectedResult,actualResult);
+        String input = null;
+        String inputWord = "se";
+        String actualResult = stringOccurrence.stringOccurrenceFinder(input, inputWord);
+        String expectedResult = null;
+        assertEquals(expectedResult, actualResult);
 
     }
+
     @Test
     public void givenAStringShouldReturnNotOccurred() {
-        String input="She sells seashells by the seashore";
-        String inputWord="zm";
-        String actualResult=stringOccurrence.stringOccurrenceFinder(input,inputWord);
-        String expectedResult="Found:0 time";
-        assertEquals(expectedResult,actualResult);
+        String input = "She sells seashells by the seashore";
+        String inputWord = "zm";
+        String actualResult = stringOccurrence.stringOccurrenceFinder(input, inputWord);
+        String expectedResult = "Found:0 time";
+        assertEquals(expectedResult, actualResult);
 
     }
 }
